8380bd542248685c75df9e3bdcb9c8eb67de719b
shiva1217 <shivangisingh1217@gmail.com>
2025-02-16 15:46:35 +0530

    final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-  authModal,
+  // authModal,
 }: {
   children: React.ReactNode;
-  authModal: React.ReactNode;
+  // authModal?: React.ReactNode;
 }) {
   return (
     <html
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
         <Providers>
           <Navbar />
-          {authModal}
+          {/* {authModal} */}
           <div className="container max-w-7xl mx-auto h-full pt-12 px-0 md:px-8">
             {children}
           </div>
