f02d34cb99d2a40f38fbfbef2fd445b3066f8fbc
shiva1217 <shivangisingh1217@gmail.com>
2025-02-16 15:25:58 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,45 +1,3 @@
-// import { Toaster } from "@/components/ui/Toaster";
-// import { cn } from "@/lib/utils";
-// import "@/app/globals.css";
-// import { Inter } from "next/font/google";
-// import { Providers } from "@/redux/provider";
-// import Navbar from "@/components/Navbar";
-// import { ReactNode } from "react";
-
-// export const metadata = {
-//   title: "BAZARIO",
-//   description:
-//     "An E-commerce website",
-// };
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: ReactNode;
-//   authModal: ReactNode;
-// }) {
-//   return (
-//     <html
-//       lang="en"
-//       className={cn(
-//         "bg-white text-slate-900 antialiased light",
-//         inter.className
-//       )}
-//     >
-//       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-//         <Providers>
-//           <Navbar />
-//           <div className="container max-w-7xl mx-auto h-full pt-12 px-0 md:px-8">
-//             {children}
-//           </div>
-//           <Toaster />
-//         </Providers>
-//       </body>
-//     </html>
-//   );
-// }
 import { Toaster } from "@/components/ui/Toaster";
 import { cn } from "@/lib/utils";
 import "@/app/globals.css";
@@ -56,11 +14,11 @@ export const metadata = {
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  // children,
-  // authModal,
+  children,
+  authModal,
 }: {
-  // children: ReactNode;
-  // authModal: ReactNode;
+  children: ReactNode;
+  authModal: ReactNode;
 }) {
   return (
     <html
@@ -73,9 +31,9 @@ export default function RootLayout({
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
         <Providers>
           <Navbar />
-          {/* {authModal} */}
+          {authModal}
           <div className="container max-w-7xl mx-auto h-full pt-12 px-0 md:px-8">
-            {/* {children} */}
+            {children}
           </div>
           <Toaster />
         </Providers>
