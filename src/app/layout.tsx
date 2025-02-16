f3ba5465ac56b8c4bea86bccd40d920b1c1fbf73
shiva1217 <shivangisingh1217@gmail.com>
2025-02-16 14:52:48 +0530

    error handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,45 @@
+// import { Toaster } from "@/components/ui/Toaster";
+// import { cn } from "@/lib/utils";
+// import "@/app/globals.css";
+// import { Inter } from "next/font/google";
+// import { Providers } from "@/redux/provider";
+// import Navbar from "@/components/Navbar";
+// import { ReactNode } from "react";
+
+// export const metadata = {
+//   title: "BAZARIO",
+//   description:
+//     "An E-commerce website",
+// };
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: ReactNode;
+//   authModal: ReactNode;
+// }) {
+//   return (
+//     <html
+//       lang="en"
+//       className={cn(
+//         "bg-white text-slate-900 antialiased light",
+//         inter.className
+//       )}
+//     >
+//       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+//         <Providers>
+//           <Navbar />
+//           <div className="container max-w-7xl mx-auto h-full pt-12 px-0 md:px-8">
+//             {children}
+//           </div>
+//           <Toaster />
+//         </Providers>
+//       </body>
+//     </html>
+//   );
+// }
 import { Toaster } from "@/components/ui/Toaster";
 import { cn } from "@/lib/utils";
 import "@/app/globals.css";
@@ -8,14 +50,14 @@ import { ReactNode } from "react";
 
 export const metadata = {
   title: "BAZARIO",
-  description:
-    "An E-commerce website",
+  description: "An E-commerce website",
 };
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: ReactNode;
   authModal: ReactNode;
@@ -31,6 +73,7 @@ export default function RootLayout({
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
         <Providers>
           <Navbar />
+          {authModal}
           <div className="container max-w-7xl mx-auto h-full pt-12 px-0 md:px-8">
             {children}
           </div>
